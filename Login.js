aa6697d248705ec01db9993192adfaec687ca240
Mihael <mihael.baresic@gmail.com>
2021-09-18 15:24:58 +0200

    Signup-form

--- Login.js
+++ Login.js
@@ -2,7 +2,7 @@ function validate(){
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
 
-    if(username=="admin" && password =="user"){
+    if(username==="admin" && password ==="user"){
         alert("Login succesfull!");
         window.location="Index.html";
     }
